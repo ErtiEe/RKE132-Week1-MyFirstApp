fe5dc45546cd52752b171523768d5398bc5ecc5e
ErtiEe <erti.eensalu@tktk.ee>
2023-01-02 17:50:41 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,8 @@
+﻿//This is a comment to my code
+Console.WriteLine("Enter your name:");
+//string - sõna
+string userName = Console.ReadLine();
+
+Console.WriteLine("Hello" + ", " + userName + "!");
+
+Console.WriteLine($"Hello, {userName} !");
